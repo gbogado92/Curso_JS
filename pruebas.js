1f2ab2ba8edfb9134702a8c57b199cb6463fd405
Gabriel Bogado <gabrielgbogado92@gmail.com>
2021-04-25 16:50:58 -0300

    Llegué hasta video 137 Fetch API

--- pruebas.js
+++ pruebas.js
@@ -1,4 +1,5 @@
 
+
 /*
 class Producto {
     constructor (nombre, precio) {
@@ -30,7 +31,6 @@ const libro = new Libro("Javascript la revolución", 120, 12373625772);
 
 console.log(libro.formatearProducto());
 console.log(Producto2.formatearProducto());
-
 
 
 class Libro {
@@ -70,7 +70,7 @@ const libro2 = new Libro2("El principito", "Jorge López", 330, 534263444);
 console.log(libro2.descripción());
 console.log(libro.descripción());
 
-<!-- PROMISE -->
+//---------------------------------VIDEO 133 PROMISE--------------------------------------------
 
 const usuarioAutenticado = new Promise( (resolve, reject) => {
     const auth = false; 
@@ -92,6 +92,8 @@ usuarioAutenticado
 // Fulfilled: Ya se cumplió
 // Rejected: Se ha rechazado o no se pudo cumplir.
 
+//---------------------------------VIDEO 134 NOTIFICATION API--------------------------------------------
+
 
 const usuarioAutenticado = new Promise( (resolve, reject) => {
     const auth = true;
@@ -108,8 +110,6 @@ usuarioAutenticado
 
 .catch( error => console.log(error))
 
-*/
-
 const boton = document.querySelector('#boton');
 boton.addEventListener('click', () => Notification.requestPermission()
 .then( (resultado) => console.log(`El resultado es: ${resultado}`))
@@ -121,6 +121,149 @@ if(Notification.permission == 'granted'){
         body: 'Código con Gabriel, los mejores tutoriales'
     })
 }
+
+*/
+
+
+//---------------------------------VIDEO 135 ASYcNC / AWAIT--------------------------------------------
+
+/*
+function descargandoClientes(){
+    return new Promise( resolve => {
+        console.log('Descargando clientes... Espere...');
+
+        setTimeout( () => {
+            resolve('Los clientes fueron descargados');
+        }, 5000 );
+    });
+}
+
+
+async function app(){
+    try {
+        const resultado = await descargandoClientes();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+app();
+
+*/
+
+//---------------------    VIDEO 136 Como trabajar con dos consultas ASYcNC / AWAIT---------------------------------
+
+function descargarNuevosClientes() {
+    return new Promise( resolve => {
+        console.log('Descargando nuevos clientes... Espere');
+
+        setTimeout( () => {
+            resolve('Los clientes fueron descargados');
+        }, 5000 );
+    });
+}
+
+
+function descargarUltimosPedidos(){
+    return new Promise( resolve => {
+        console.log('Descargando ultimos pedidos');
+
+        setTimeout( ()=> {
+            resolve('Los pedidos fueron descargados');
+        }, 3000 );
+    });
+}
+
+async function App() {
+    try {
+        // const clientes = await descargarNuevosClientes();
+        // const pedidos = await descargarUltimosPedidos();
+        // console.log(pedidos);
+        // console.log(clientes);
+        const resultado = await Promise.all([ descargarNuevosClientes(), descargarUltimosPedidos() ]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+App();
+
+
+
+
+
+
+
+
+
+
+
+
+/* ----------------------------------------PRACTICAS CLASES--------------------------------------------
+
+class Producto {
+    constructor(nombre,precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    instrucciones(){
+        return `El producto ${this.nombre} sale $${this.precio} c/u`;
+    }
+}
+
+const producto = new Producto('Sika Swell', '1.500,00')
+
+producto.instrucciones();
+
+class Aditivo extends Producto {
+    constructor(nombre,precio,codigo){
+        super(nombre,precio);
+        this.codigo = codigo;
+    }
+    instrucciones(){
+        return `${super.instrucciones()} y su codigo es: ${this.codigo}`;
+    }
+}
+
+const aditivo = new Aditivo('Adva Cast 312', '189,00', 1254545245);
+
+console.log(aditivo.instrucciones());
+ */
+
+
+// ----------------------------------------PRACTICAS NOTIFICATION API--------------------------------------------
+/*
+const usuarioAutenticado = new Promise ( (resolve, reject) => {
+    const auth = true;
+
+    if(auth) {
+        resolve('Usuario autenticado')
+    } else {
+        reject('Usuario no autenticado')
+    }
+})
+
+usuarioAutenticado
+.then( resultado => console.log(resultado))
+.catch( error => console.log(error))
+
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', () => Notification.requestPermission()
+.then( resultado => console.log(`El resultado es: ${resultado}`))
+)
+
+if(Notification.permission == 'granted'){
+    new Notification('Esta es una notificacion', {
+        icon: 'img/ccj.png',
+        body: 'Codigo con Gaby, los mejores tutoriales'
+    })
+}
+*/
+
+
 
 
 
